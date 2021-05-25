9fae30ace00edcde6e34baa0293ed0ec802bebf5
thatrafi <muhammad.rafiudin@students.amikom.ac.id>
2021-05-25 15:14:52 +0700

    add pengguna model

---
+++ sppenyakitlambung/Models/Pengguna.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace sppenyakitlambung.Models
+{
+    public class Pengguna
+    {
+        public string _id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string gender { get; set; }
+        public int umur { get; set; }
+        public string phone_number { get; set; }
+        public string address { get; set; }
+        public string status { get; set; }
+        public string createdAt { get; set; }
+        public string updatedAt { get; set; }
+    }
+}
--- sppenyakitlambung/ViewModel/RegisterViewModel.cs
+++ sppenyakitlambung/ViewModel/RegisterViewModel.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using sppenyakitlambung.Models;
 using Xamarin.Forms;
 
 namespace sppenyakitlambung.ViewModel
 {
-    public class RegisterViewModel
+    public class RegisterViewModel : BaseViewModel
     {
-        
+        private Pengguna pengguna;
 
         public RegisterViewModel()
         {
-
+            pengguna = new Pengguna();
         }
 
-       
+        public Pengguna Pengguna {
+            get => pengguna;
+            set {
+                pengguna = value;
+                RaisePropertyChanged(nameof(Pengguna));
+            }
+        }
     }
 }
